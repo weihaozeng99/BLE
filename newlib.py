b524a894c3d54f23b68efff28a3bb078a6e92308
weihaozeng99 <weihao_zeng@brown.edu>
2023-06-22 22:41:34 -0400

    fixed bugs

--- newlib.py
+++ newlib.py
@@ -6,14 +6,18 @@ esp_addr="4FAFC201-1FB5-459E-8FCC-C5C9331914B"
 esp_name="Long name works now"
 esp_test_UUID="42c02b73-0b43-492e-8941-91a547a180f0"
 async def main():
+    print("Finding device now\n")
     devices = await BleakScanner.find_device_by_name(esp_name)
     print(devices.name)
-    async with BleakClient(devices) as client:
-            #print(client.address)
-        data= await client.read_gatt_char(esp_test_UUID)
-        print(int.from_bytes(data,"little"))
+    if devices != None:
+        print("Connecting")
+        async with BleakClient(devices) as client:
+                #print(client.address)
+            data= await client.read_gatt_char(esp_test_UUID)
+            print(int.from_bytes(data,"little"))
        #await client.disconnect()
-    
+    else:
+        print("ERROR: No device was found!\n")
 
 while 1:
     asyncio.run(main())
